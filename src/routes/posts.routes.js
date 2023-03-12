4e33f05a7838346f887a95326e80a32cf1bc7964
Felipe Carvalho <felipe_pc7@hotmail.com>
2023-03-12 14:08:52 -0300

    done routes

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -1,28 +1,59 @@
 const express = require("express");
 const router = express.Router();
-
-const postList = [
-    {
-        id: 1,
-        title: "post 1",
-        content: "content 1",
-    },
-    {
-        id: 2,
-        title: "post 2",
-        content: "content 2",
-    },
-];
+const { posts } = require("../models");
 
 // Create
-router.post("/create", (req, res) => {
-    postList.push(req.body);
-    res.status(201).send(postList);
+router.post("/posts-create", async (req, res) => {
+    const { title, content } = req.body;
+    await posts.create({ title, content });
+    res.status(201).send("criado com sucesso");
 });
 
 // Read
-router.get("/posts", (req, res) => {
-    res.status(200).send(postList);
+router.get("/posts", async (req, res) => {
+    const allPosts = await posts.findAll();
+    res.status(200).send(allPosts);
+});
+
+// Update
+router.put("/posts-update/:id", async (req, res) => {
+    const { title, content } = req.body;
+    const { id } = req.params;
+
+    const post = await posts.findOne({ where: { id } });
+    if (!post) {
+        res.status(401).send({ message: "post não foi encontrado..." });
+    } else {
+        await posts.update(
+            {
+                title,
+                content,
+            },
+            {
+                where: {
+                    id,
+                },
+            }
+        );
+
+        res.status(200).send({ message: "post alterado!" });
+    }
+});
+
+// Delete
+router.delete("/posts-delete/:id", async (req, res) => {
+    const { id } = req.params;
+    const post = await posts.findOne({ where: { id } });
+    if (!post) {
+        res.status(401).send({ message: "post não foi encontrado..." });
+    } else {
+        await posts.destroy({
+            where: {
+                id,
+            },
+        });
+        res.status(200).send({ message: "deletado com sucesso" });
+    }
 });
 
 module.exports = router;
